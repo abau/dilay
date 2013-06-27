8a73b7d717a94a7ec923106c71eae4c6687ce13b
abau <dev@abau.org>
2013-06-27 23:03:10 +0200

    provide Id constructor with initialization to explicit value

--- src/id.cpp
+++ src/id.cpp
@@ -1,6 +1,6 @@
 #include "id.hpp"
 
-Id :: Id ()          : _id (Id::nextId ()) {}
+Id :: Id (IdType id) : _id (id == Id::invalidId () ? Id::nextId () : id) {}
 Id :: Id (const Id&) : _id (Id::nextId ()) {}
 
 const Id& Id :: operator= (const Id&) {
@@ -8,7 +8,7 @@ const Id& Id :: operator= (const Id&) {
 }
 
 IdType Id :: nextId () {
-  static IdType nextId = 0;
+  static IdType nextId = Id::invalidId ();
   nextId++;
   return nextId;
 }
--- src/id.hpp
+++ src/id.hpp
@@ -4,11 +4,12 @@
 typedef unsigned long IdType;
 
 class Id {
-  public:     Id        ();
+  public:     Id        (IdType = Id::invalidId ());
               Id        (const Id&);
     const Id& operator= (const Id&);
 
-    IdType    get       () const { return this->_id; }
+           IdType get       () const { return this->_id; }
+    static IdType invalidId ()       { return 0; }
 
   private:
     const  IdType _id;
