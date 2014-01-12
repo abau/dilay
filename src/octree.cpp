60d1e0438b500f871c451da9a9f2ac650ab20fff
abau <dev@abau.org>
2014-01-12 12:53:13 +0100

    render empty octree nodes as well

--- src/octree.cpp
+++ src/octree.cpp
@@ -118,14 +118,13 @@ struct OctreeNode::Impl {
 
   void render () {
 #ifdef DILAY_RENDER_OCTREE
-    if (this->faces.size () > 0) {
-      this->mesh.renderBegin ();
-      glDisable (GL_DEPTH_TEST);
-      Renderer :: setColor3 (Color (1.0f, 1.0f, 0.0f));
-      glDrawElements (GL_LINES, this->mesh.numIndices (), GL_UNSIGNED_INT, (void*)0);
-      glEnable (GL_DEPTH_TEST);
-      this->mesh.renderEnd ();
-    }
+    this->mesh.renderBegin ();
+    glDisable (GL_DEPTH_TEST);
+    Renderer :: setColor3 (Color (1.0f, 1.0f, 0.0f));
+    glDrawElements (GL_LINES, this->mesh.numIndices (), GL_UNSIGNED_INT, (void*)0);
+    glEnable (GL_DEPTH_TEST);
+    this->mesh.renderEnd ();
+
     for (Child& c : this->children) 
       c->render ();
 #else
