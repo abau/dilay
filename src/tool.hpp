48dd9e7deac878f63b9d6f4170fe2cf75f288dee
abau <dev@abau.org>
2014-03-18 18:01:16 +0100

    add tool macros

--- src/tool.hpp
+++ src/tool.hpp
@@ -32,16 +32,59 @@ class Tool {
     virtual bool runMousePressEvent (QMouseEvent*) { return false; }
 };
 
-#define DECLARE_TOOL(t)                                   \
-  DECLARE_BIG3 ( t , ViewMainWindow*, QContextMenuEvent*) \
+#define DECLARE_TOOL(t,...)                                              \
+  DECLARE_BIG3 ( t , ViewMainWindow*, QContextMenuEvent*, ##__VA_ARGS__) \
   static QString toolName ();
+
+#define DELEGATE_TOOL_WITHOUT_CONSTRUCTOR(t)          \
+  DELEGATE_BIG3_WITHOUT_CONSTRUCTOR_SELF ( t )        \
+  DELEGATE_STATIC (QString, t , toolName)
 
 #define DELEGATE_TOOL(t)                              \
   t :: t (ViewMainWindow* mW, QContextMenuEvent* mE)  \
     : Tool (mW, mE, t :: toolName ()) {               \
         DELEGATE_NEW_IMPL (this)                      \
     }                                                 \
-  DELEGATE_BIG3_WITHOUT_CONSTRUCTOR_SELF ( t )        \
-  DELEGATE_STATIC (QString, t , toolName)
+  DELEGATE_TOOL_WITHOUT_CONSTRUCTOR( t )
+
+#define DELEGATE1_TOOL(t,t1)                          \
+  t :: t ( ViewMainWindow* mW, QContextMenuEvent* mE  \
+         , t1 a1, t2 a2 )                             \
+    : Tool (mW, mE, t :: toolName ()) {               \
+        DELEGATE_NEW_IMPL (this,a1,a2)                \
+    }                                                 \
+  DELEGATE_TOOL_WITHOUT_CONSTRUCTOR( t )
+
+#define DELEGATE2_TOOL(t,t1,t2)                       \
+  t :: t ( ViewMainWindow* mW, QContextMenuEvent* mE  \
+         , t1 a1, t2 a2 )                             \
+    : Tool (mW, mE, t :: toolName ()) {               \
+        DELEGATE_NEW_IMPL (this,a1,a2)                \
+    }                                                 \
+  DELEGATE_TOOL_WITHOUT_CONSTRUCTOR( t )
+
+#define DELEGATE3_TOOL(t,t1,t2,t3)                    \
+  t :: t ( ViewMainWindow* mW, QContextMenuEvent* mE  \
+         , t1 a1, t2 a2, t3 a3 )                      \
+    : Tool (mW, mE, t :: toolName ()) {               \
+        DELEGATE_NEW_IMPL (this,a1,a2,a3)             \
+    }                                                 \
+  DELEGATE_TOOL_WITHOUT_CONSTRUCTOR( t )
+
+#define DELEGATE4_TOOL(t,t1,t2,t3,t4)                 \
+  t :: t ( ViewMainWindow* mW, QContextMenuEvent* mE  \
+         , t1 a1, t2 a2, t3 a3, t4 a4 )               \
+    : Tool (mW, mE, t :: toolName ()) {               \
+        DELEGATE_NEW_IMPL (this,a1,a2,a3,a4)          \
+    }                                                 \
+  DELEGATE_TOOL_WITHOUT_CONSTRUCTOR( t )
+
+#define DELEGATE5_TOOL(t,t1,t2,t3,t4,t5)              \
+  t :: t ( ViewMainWindow* mW, QContextMenuEvent* mE  \
+         , t1 a1, t2 a2, t3 a3, t4 a4, t5 a5 )        \
+    : Tool (mW, mE, t :: toolName ()) {               \
+        DELEGATE_NEW_IMPL (this,a1,a2,a3,a4,a5)       \
+    }                                                 \
+  DELEGATE_TOOL_WITHOUT_CONSTRUCTOR( t )
 
 #endif
