83dd668fee5288fb724773bd2d3aac642912b802
abau <dev@abau.org>
2014-05-18 15:20:24 +0200

    add config proxy

--- lib/src/config.hpp
+++ lib/src/config.hpp
@@ -1,6 +1,7 @@
 #ifndef DILAY_CONFIG
 #define DILAY_CONFIG
 
+#include <string>
 #include "macro.hpp"
 
 class Config {
@@ -18,6 +19,31 @@ class Config {
 
     class Impl;
     Impl* impl;
+};
+
+class ConfigProxy {
+  public:
+    ConfigProxy (const std::string& p) : prefix (p) {
+      assert (p.back () == '/');
+    }
+
+    template <class T> 
+    const T& get (const std::string& p) { 
+      return Config::get<T> (this->prefix + p);
+    }
+
+    template <class T> 
+    const T& get (const std::string& p, const T& v) {
+      return Config::get<T> (this->prefix + p, v);
+    }
+
+    template <class T> 
+    void cache (const std::string& p, const T& v) {
+      return Config::cache<T> (this->prefix + p, v);
+    }
+
+  private:
+    std::string prefix;
 };
 
 #endif
