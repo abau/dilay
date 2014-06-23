bfd2f0a2e79f3c17f2f63654732e050a68d6f3fb
abau <dev@abau.org>
2014-06-23 16:59:24 +0200

    add action transformers

--- lib/src/action/unit.hpp
+++ lib/src/action/unit.hpp
@@ -2,17 +2,25 @@
 #define DILAY_ACTION_UNIT
 
 #include "action.hpp"
+#include "action/transformer.hpp"
 #include "macro.hpp"
 
 class ActionUnit : public Action {
   public: 
     DECLARE_BIG3 (ActionUnit)
 
-    template <class T>
-    T& add () { 
-      T& action = *new T ();
+    template <class A>
+    A& add () { 
+      A& action = *new A ();
       this->addAction (action); 
       return action; 
+    }
+
+    template <typename A, typename T>
+    A& add (T& t) { 
+      A& action = *new A ();
+      this->addAction (*new ActionTransformer (t, action));
+      return *action; 
     }
 
     bool isEmpty () const;
