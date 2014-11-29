19b96cb2cb7f9598a5be48a6a114b8f4345264c0
abau <dev@abau.org>
2014-11-29 12:00:22 +0100

    const correctness for affected faces

--- lib/src/affected-faces.hpp
+++ lib/src/affected-faces.hpp
@@ -8,20 +8,21 @@ class AffectedFaces {
   public:
     DECLARE_BIG6 (AffectedFaces)
 
-    void         insert          (WingedFace&);
-    void         insert          (const AffectedFaces&);
-    void         remove          (WingedFace&);
-    void         remove          (WingedEdge&);
-    void         reset           ();
-    void         commit          ();
-    bool         contains        (WingedFace&) const;
-    bool         contains        (WingedFace*) const;
-    bool         contains        (WingedEdge&) const;
-    bool         contains        (WingedEdge*) const;
-    VertexPtrSet toVertexSet     () const;
-    FacePtrSet&  faces           () const;
-    FacePtrSet&  uncommitedFaces () const;
-    EdgePtrSet&  edges           () const;
+    void insert   (WingedFace&);
+    void insert   (const AffectedFaces&);
+    void remove   (WingedFace&);
+    void remove   (WingedEdge&);
+    void reset    ();
+    void commit   ();
+    bool contains (WingedFace&) const;
+    bool contains (WingedFace*) const;
+    bool contains (WingedEdge&) const;
+    bool contains (WingedEdge*) const;
+
+          VertexPtrSet toVertexSet     () const;
+    const FacePtrSet&  faces           () const;
+    const FacePtrSet&  uncommitedFaces () const;
+    const EdgePtrSet&  edges           () const;
 
   private:
     IMPLEMENTATION
