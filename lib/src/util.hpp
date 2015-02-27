1e81b5c5f008c88c2ef8bf2f34cfaebd757ed455
abau <dev@abau.org>
2015-02-27 07:11:03 +0100

    fix indentation

--- lib/src/util.hpp
+++ lib/src/util.hpp
@@ -10,14 +10,14 @@ std::ostream& operator<<(std::ostream&, const glm::vec3&);
 std::ostream& operator<<(std::ostream&, const glm::vec4&);
 
 namespace Util {
-  glm::vec3       between            (const glm::vec3&, const glm::vec3&);
-  glm::vec3       transformPosition  (const glm::mat4x4&, const glm::vec3&);
-  glm::vec3       transformDirection (const glm::mat4x4&, const glm::vec3&);
-  std::string     readFile           (const std::string&); 
-  unsigned int    solveQuadraticEq   (float, float, float, float&, float&);
-  float           epsilon            ();
-  bool            isNaN              (const glm::vec3&);
-  float           defaultScale       ();
+  glm::vec3    between            (const glm::vec3&, const glm::vec3&);
+  glm::vec3    transformPosition  (const glm::mat4x4&, const glm::vec3&);
+  glm::vec3    transformDirection (const glm::mat4x4&, const glm::vec3&);
+  std::string  readFile           (const std::string&); 
+  unsigned int solveQuadraticEq   (float, float, float, float&, float&);
+  float        epsilon            ();
+  bool         isNaN              (const glm::vec3&);
+  float        defaultScale       ();
 }
 
 #endif
