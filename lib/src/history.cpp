f45d5425e7bccd53c9fe89ac0226a87aa1dc678a
abau <dev@abau.org>
2015-05-19 16:18:44 +0200

    save octrees in history

--- lib/src/history.cpp
+++ lib/src/history.cpp
@@ -1,28 +1,57 @@
 #include <list>
+#include <vector>
 #include "config.hpp"
 #include "history.hpp"
+#include "maybe.hpp"
 #include "mesh.hpp"
+#include "octree.hpp"
 #include "scene.hpp"
 #include "winged/mesh.hpp"
 
 namespace {
-  typedef std::list <Mesh>          SceneSnapshot;
+  struct MeshSnapshot {
+    Mesh           mesh;
+    Maybe <Octree> octree;
+  };
+
+  typedef std::list <MeshSnapshot>  SceneSnapshot;
   typedef std::list <SceneSnapshot> Timeline;
 
-  SceneSnapshot sceneSnapshot (const Scene& scene) {
+  SceneSnapshot sceneSnapshot (const Scene& scene, bool copyOctree) {
     SceneSnapshot snapshot;
 
-    scene.forEachConstMesh ([&snapshot] (const WingedMesh& mesh) {
-      snapshot.push_front (mesh.makePrunedMesh ());
+    scene.forEachConstMesh ([copyOctree, &snapshot] (const WingedMesh& mesh) {
+      if (copyOctree) {
+        std::vector <unsigned int> newFaceIndices;
+
+        MeshSnapshot meshSnapshot = { mesh.makePrunedMesh (&newFaceIndices)
+                                    , mesh.octree () };
+
+        meshSnapshot.octree->mesh (meshSnapshot.mesh);
+
+        if (newFaceIndices.empty () == false) {
+          meshSnapshot.octree->rewriteIndices (newFaceIndices);
+        }
+        snapshot.push_front (std::move (meshSnapshot));
+      }
+      else {
+        snapshot.push_front ({ mesh.makePrunedMesh (), Maybe <Octree> () });
+      }
     });
     return snapshot;
+  }
+
+  void deleteOctreeSnapshot (SceneSnapshot& sceneSnapshot) {
+    for (MeshSnapshot& meshSnapshot : sceneSnapshot) {
+      meshSnapshot.octree.reset ();
+    }
   }
 }
 
 struct History::Impl {
+  unsigned int undoDepth;
   Timeline     past;
   Timeline     future;
-  unsigned int undoDepth;
 
   Impl (const Config& config) {
     this->runFromConfig (config);
@@ -35,7 +64,10 @@ struct History::Impl {
       while (this->past.size () >= this->undoDepth) {
         this->past.pop_back ();
       }
-      this->past.push_front (std::move (sceneSnapshot (scene)));
+      if (this->past.empty () == false) {
+        deleteOctreeSnapshot (this->past.front ());
+      }
+      this->past.push_front (std::move (sceneSnapshot (scene, true)));
     }
   }
 
@@ -48,14 +80,14 @@ struct History::Impl {
   void resetToSnapshot (const SceneSnapshot& snapshot, Scene& scene) {
     scene.reset ();
 
-    for (const Mesh& mesh : snapshot) {
-      scene.newWingedMesh (mesh);
+    for (const MeshSnapshot& meshSnapshot : snapshot) {
+      scene.newWingedMesh (meshSnapshot.mesh);
     }
   }
 
   void undo (Scene& scene) {
     if (this->past.empty () == false) {
-      this->future.push_front (std::move (sceneSnapshot (scene)));
+      this->future.push_front (std::move (sceneSnapshot (scene, false)));
       this->resetToSnapshot (this->past.front (), scene);
       this->past.pop_front ();
     }
@@ -63,9 +95,24 @@ struct History::Impl {
 
   void redo (Scene& scene) {
     if (this->future.empty () == false) {
-      this->past.push_front (std::move (sceneSnapshot (scene)));
+      if (this->past.empty () == false) {
+        deleteOctreeSnapshot (this->past.front ());
+      }
+      this->past.push_front (std::move (sceneSnapshot (scene, false)));
       this->resetToSnapshot (this->future.front (), scene);
       this->future.pop_front ();
+    }
+  }
+
+  bool hasRecentOctrees () const {
+    return this->past.empty () == false && bool (this->past.front ().front ().octree);
+  }
+
+  void forEachRecentOctree (const std::function <void (const Octree&)> f) const {
+    assert (this->hasRecentOctrees ());
+    for (const MeshSnapshot& s : this->past.front ()) {
+      assert (s.octree);
+      f (s.octree.getRef ());
     }
   }
 
@@ -74,9 +121,11 @@ struct History::Impl {
   }
 };
 
-DELEGATE1_BIG3 (History, const Config&)
-DELEGATE1 (void, History, snapshot, const Scene&)
-DELEGATE  (void, History, dropSnapshot)
-DELEGATE1 (void, History, undo, Scene&)
-DELEGATE1 (void, History, redo, Scene&)
-DELEGATE1 (void, History, runFromConfig, const Config&)
+DELEGATE1_BIG3  (History, const Config&)
+DELEGATE1       (void, History, snapshot, const Scene&)
+DELEGATE        (void, History, dropSnapshot)
+DELEGATE1       (void, History, undo, Scene&)
+DELEGATE1       (void, History, redo, Scene&)
+DELEGATE1       (void, History, runFromConfig, const Config&)
+DELEGATE_CONST  (bool, History, hasRecentOctrees)
+DELEGATE1_CONST (void, History, forEachRecentOctree, const std::function <void (const Octree&)>)
--- lib/src/history.hpp
+++ lib/src/history.hpp
@@ -4,16 +4,19 @@
 #include "configurable.hpp"
 #include "macro.hpp"
 
+class Octree;
 class Scene;
 
 class History : public Configurable {
   public: 
     DECLARE_BIG3 (History, const Config&)
 
-    void snapshot     (const Scene&);
-    void dropSnapshot ();
-    void undo         (Scene&);
-    void redo         (Scene&);
+    void snapshot            (const Scene&);
+    void dropSnapshot        ();
+    void undo                (Scene&);
+    void redo                (Scene&);
+    bool hasRecentOctrees    () const;
+    void forEachRecentOctree (const std::function <void (const Octree&)>) const;
 
   private:
     IMPLEMENTATION
