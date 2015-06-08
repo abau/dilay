ac7c150f5862b702d1f23fd689f08671f1f33a3a
abau <dev@abau.org>
2015-06-08 20:45:45 +0200

    delete test cases

--- lib/src/mesh-util.hpp
+++ lib/src/mesh-util.hpp
@@ -7,18 +7,15 @@ class Mesh;
 class PrimPlane;
 
 namespace MeshUtil {
-  Mesh cube                ();
-  Mesh sphere              (unsigned int, unsigned int);
-  Mesh icosphere           (unsigned int);
-  Mesh cone                (unsigned int);
-  Mesh cylinder            (unsigned int);
-  Mesh testCaseValcence3_1 ();
-  Mesh testCaseValcence3_2 ();
-  Mesh testCaseValcence3_3 ();
+  Mesh cube             ();
+  Mesh sphere           (unsigned int, unsigned int);
+  Mesh icosphere        (unsigned int);
+  Mesh cone             (unsigned int);
+  Mesh cylinder         (unsigned int);
 
-  Mesh mirror              (const Mesh&, const PrimPlane&);
-  bool checkConsistency    (const Mesh&);
-  void toObjFile           (std::ostream&, const Mesh&);
+  Mesh mirror           (const Mesh&, const PrimPlane&);
+  bool checkConsistency (const Mesh&);
+  void toObjFile        (std::ostream&, const Mesh&);
 };
 
 #endif
