55c2c8a9c525b38b71dc0b736f9b0150405a8eae
abau <dev@abau.org>
2015-06-29 15:29:01 +0200

    add german translation

--- i18n/dilay_de.ts
+++ i18n/dilay_de.ts
@@ -2,11 +2,266 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
 <context>
-    <name>ViewToolbar</name>
+    <name>QObject</name>
     <message>
-        <location filename="../src/view/toolbar.cpp" line="12"/>
-        <source>Scene</source>
-        <translation>Szene</translation>
+        <source>Delete selection</source>
+        <translation>Auswahl löschen</translation>
+    </message>
+    <message>
+        <source>X-axis</source>
+        <translation>X-Achse</translation>
+    </message>
+    <message>
+        <source>Y-axis</source>
+        <translation>Y-Achse</translation>
+    </message>
+    <message>
+        <source>Z-axis</source>
+        <translation>Z-Achse</translation>
+    </message>
+    <message>
+        <source>XY-plane</source>
+        <translation>XY-Ebene</translation>
+    </message>
+    <message>
+        <source>XZ-plane</source>
+        <translation>XZ-Ebene</translation>
+    </message>
+    <message>
+        <source>YZ-plane</source>
+        <translation>YZ-Ebene</translation>
+    </message>
+    <message>
+        <source>Camera-plane</source>
+        <translation>Kamera-Ebene</translation>
+    </message>
+    <message>
+        <source>Primary plane</source>
+        <translation>Primäre Ebene</translation>
+    </message>
+    <message>
+        <source>Drag to move</source>
+        <translation>Ziehen zum Verschieben</translation>
+    </message>
+    <message>
+        <source>Drag to move orthogonally</source>
+        <translation>Ziehen zum senkrechten Verschieben</translation>
+    </message>
+    <message>
+        <source>Radius</source>
+        <translation>Radius</translation>
+    </message>
+    <message>
+        <source>Subdivide</source>
+        <translation>Unterteilen</translation>
+    </message>
+    <message>
+        <source>Sync</source>
+        <translation>Sync</translation>
+    </message>
+    <message>
+        <source>Mirror</source>
+        <translation>Spiegeln</translation>
+    </message>
+    <message>
+        <source>Change radius</source>
+        <translation>Radius ändern</translation>
+    </message>
+    <message>
+        <source>Drag to sculpt</source>
+        <translation>Ziehen zum Verformen</translation>
+    </message>
+    <message>
+        <source>Drag to sculpt inverted</source>
+        <translation>Ziehen zum invertierten Verformen</translation>
+    </message>
+    <message>
+        <source>Intensity</source>
+        <translation>Intensität</translation>
+    </message>
+    <message>
+        <source>Invert</source>
+        <translation>Invertieren</translation>
+    </message>
+    <message>
+        <source>Inflate</source>
+        <translation>Aufblähen</translation>
+    </message>
+    <message>
+        <source>Smoothness</source>
+        <translation>Glattheit</translation>
+    </message>
+    <message>
+        <source>Along primary plane</source>
+        <translation>Entlang der primären Ebene</translation>
+    </message>
+    <message>
+        <source>Discard backfaces</source>
+        <translation>Rückseiten verwerfen</translation>
+    </message>
+    <message>
+        <source>Relax only</source>
+        <translation>Nur entspannen</translation>
+    </message>
+    <message>
+        <source>New mesh</source>
+        <translation>Neues Mesh</translation>
+    </message>
+    <message>
+        <source>Delete mesh</source>
+        <translation>Mesh löschen</translation>
+    </message>
+    <message>
+        <source>Move mesh</source>
+        <translation>Mesh verschieben</translation>
+    </message>
+    <message>
+        <source>Carve</source>
+        <translation>Formen</translation>
+    </message>
+    <message>
+        <source>Crease</source>
+        <translation>Falzen</translation>
+    </message>
+    <message>
+        <source>Grab</source>
+        <translation>Greifen</translation>
+    </message>
+    <message>
+        <source>Drag</source>
+        <translation>Ziehen</translation>
+    </message>
+    <message>
+        <source>Flatten</source>
+        <translation>Abflachen</translation>
+    </message>
+    <message>
+        <source>Smooth</source>
+        <translation>Glätten</translation>
+    </message>
+    <message>
+        <source>Pinch</source>
+        <translation>Zusammendrücken</translation>
+    </message>
+    <message>
+        <source>Reduce</source>
+        <translation>Reduzieren</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Datei</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;Bearbeiten</translation>
+    </message>
+    <message>
+        <source>&amp;View</source>
+        <translation>&amp;Ansicht</translation>
+    </message>
+    <message>
+        <source>&amp;Open...</source>
+        <translation>Ö&amp;ffnen...</translation>
+    </message>
+    <message>
+        <source>Open...</source>
+        <translation>Öffnen...</translation>
+    </message>
+    <message>
+        <source>Could not open file.</source>
+        <translation>Datei konnte nicht geöffnet werden.</translation>
+    </message>
+    <message>
+        <source>Save &amp;as...</source>
+        <translation>Speichern &amp;unter...</translation>
+    </message>
+    <message>
+        <source>Save as...</source>
+        <translation>Speichern unter...</translation>
+    </message>
+    <message>
+        <source>Could not save to file.</source>
+        <translation>Datei konnte nicht gespeichert werden.</translation>
+    </message>
+    <message>
+        <source>&amp;Save</source>
+        <translation>&amp;Speichern</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Beenden</translation>
+    </message>
+    <message>
+        <source>Do you want to quit?</source>
+        <translation>Wollen Sie wirklich beenden?</translation>
+    </message>
+    <message>
+        <source>&amp;Undo</source>
+        <translation>&amp;Rückgängig</translation>
+    </message>
+    <message>
+        <source>&amp;Redo</source>
+        <translation>&amp;Wiederherstellen</translation>
+    </message>
+    <message>
+        <source>&amp;Snap camera</source>
+        <translation>&amp;Kamera einrasten</translation>
+    </message>
+    <message>
+        <source>Reset &amp;gaze point</source>
+        <translation>&amp;Blickpunkt zurücksetzen</translation>
+    </message>
+    <message>
+        <source>Toggle &amp;wireframe</source>
+        <translation>&amp;Drahtgitter umschalten</translation>
+    </message>
+    <message>
+        <source>Toggle &amp;shading</source>
+        <translation>&amp;Schattierung umschalten</translation>
+    </message>
+    <message>
+        <source>Drag to rotate</source>
+        <translation>Ziehen zum Rotieren</translation>
+    </message>
+    <message>
+        <source>Drag to pan</source>
+        <translation>Ziehen zum Schwenken</translation>
+    </message>
+    <message>
+        <source>Gaze</source>
+        <translation>Zentrieren</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <source>Ctrl</source>
+        <translation>Strg</translation>
+    </message>
+    <message>
+        <source>Shift</source>
+        <translation>Umschalt</translation>
+    </message>
+    <message>
+        <source>Alt</source>
+        <translation>Alt</translation>
+    </message>
+    <message>
+        <source>Left</source>
+        <translation>Links</translation>
+    </message>
+    <message>
+        <source>Middle</source>
+        <translation>Mitte</translation>
+    </message>
+    <message>
+        <source>Wheel</source>
+        <translation>Rad</translation>
+    </message>
+    <message>
+        <source>Right</source>
+        <translation>Rechts</translation>
     </message>
 </context>
 </TS>
