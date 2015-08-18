124499396a90fa6341ee140add5ed551ba2909d8
abau <dev@abau.org>
2015-08-18 16:35:53 +0200

    delete unnecessary config attribute

--- lib/src/tool.cpp
+++ lib/src/tool.cpp
@@ -19,14 +19,12 @@
 struct Tool::Impl {
   Tool*       self;
   State&      state;
-  Config&     config;
   CacheProxy _cache;
 
   Impl (Tool* s, State& st, const char* key) 
-    : self           (s) 
-    , state          (st)
-    , config         (this->state.config ())
-    ,_cache          (this->cache (key))
+    : self  (s) 
+    , state (st)
+    ,_cache (this->cache (key))
   {
     this->state.mainWindow ().showToolTip (ViewToolTip ());
   }
@@ -69,6 +67,10 @@ struct Tool::Impl {
 
   void showToolTip (const ViewToolTip& toolTip) {
     this->state.mainWindow ().showToolTip (toolTip);
+  }
+
+  Config& config () const {
+    return this->state.config ();
   }
 
   CacheProxy& cache () {
@@ -129,7 +131,7 @@ GETTER_CONST    (State&         , Tool, state)
 DELEGATE        (void           , Tool, updateGlWidget)
 DELEGATE_CONST  (ViewProperties&, Tool, properties)
 DELEGATE1       (void           , Tool, showToolTip, const ViewToolTip&)
-GETTER_CONST    (Config&        , Tool, config)
+DELEGATE_CONST  (Config&        , Tool, config)
 DELEGATE        (CacheProxy&    , Tool, cache)
 DELEGATE1_CONST (CacheProxy     , Tool, cache, const char*)
 DELEGATE_CONST  (glm::ivec2     , Tool, cursorPosition)
